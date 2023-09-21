0a17be07659bc2501b0cbcc3f9fdc1e43651a5e5
JendareyTechnologies <ak@jendareyconsulting.com>
2023-09-21 18:50:14 -0500

    This is my first commit

---
+++ src/main/java/com/jendareytech/introtogit/IntrotogitApplication.java
@@ -0,0 +1,13 @@
+package com.jendareytech.introtogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrotogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrotogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jendareytech/introtogit/IntrotogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jendareytech.introtogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrotogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
